994444659d1d424c9c383bb6bc5bd5abd2ae4114
haswanth <vangal.haswanth99@gmail.com>
2021-07-28 18:18:55 +0530

    'userdashboard and admindashboard created and integrated'

--- src/app/components/admindashboard/admindashboard.component.ts
+++ src/app/components/admindashboard/admindashboard.component.ts
@@ -1,4 +1,32 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FormBuilder} from "@angular/forms";
+
+export interface StockExchange{
+  id:number
+  name:string
+  street:string
+  state:string
+  country:string
+  brief:string
+  remarks:string
+}
+
+export interface  Sector{
+  id:number
+  name:string
+  brief:string
+}
+
+export interface Company{
+  id:number
+  code:number
+  turnover:number
+  ceo:string
+  brief:string
+  name:string
+  sector:Sector
+}
 
 @Component({
   selector: 'app-admindashboard',
@@ -7,10 +35,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AdmindashboardComponent implements OnInit {
 
-  constructor() {
+  constructor(private httpClient:HttpClient,
+              private formBuilder:FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.httpClient.get("http://localhost:8080/admin/getallexchanges")
+      .subscribe(data=> {
+        this.stockExchangeList = <StockExchange[]>data
+      })
+
+    this.httpClient.get("http://localhost:8080/admin/getallcompanies")
+      .subscribe(data=> {
+        this.companyList = <Company[]>data
+      })
+    this.httpClient.get("http://localhost:8080/admin/getallsectors")
+      .subscribe(data=> {
+        this.sectorList = <Sector[]>data
+      })
   }
 
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -17,6 +17,7 @@ export class SignupComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
   }
 
   onSubmit(data: any) {
